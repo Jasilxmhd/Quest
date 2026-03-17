bfe67b10c9e6e69e1874fded09e8ea90fe0d7176
Jasilxmhd <jasilmuhammed25@gmail.com>
2026-03-17 23:18:16 +0530

    17 Mar 26

---
+++ 6 - python/Module_2/Data_structure/List.py
@@ -0,0 +1,47 @@
+# List 
+
+# details = ['Jasil',21,'Calicut']
+# print(details)
+
+
+
+
+# sample_list = []
+# print(type(sample_list))
+
+
+
+
+# sample_list2 = list()
+# print(type(sample_list2))
+
+
+
+
+
+# sample = [1,2,3,'Jasil',10.25,[10,20,30],True,None]
+# print(sample)
+
+
+
+
+
+# list1 = list('Jasil')                 # ['J', 'a', 's', 'i', 'l']
+# list1 = list([1,2,3])                 # [1, 2, 3]
+# list1 = list(1)     
+#                   # Error                              
+# print(list1)
+
+
+
+
+
+# Access in a List Element 
+
+address = [1,2,3,'Jasil',[0,6,7,5],True]
+
+print(address[3])                           # Jasil
+print(address[5])                           # True
+
+
+
