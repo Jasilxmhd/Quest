dd4257a587171ebbe89d94df4655e630d604fbe7
Jasilxmhd <jasilmuhammed25@gmail.com>
2026-05-08 12:52:20 +0530

    8 May 26

---
+++ 6_python/Module_5/os_module.py
@@ -0,0 +1,30 @@
+
+'OS Module'
+
+import os 
+# print(os.path.exists('jasil.txt'))         # File ndoon nookaaan
+
+# os.rename("jasil.txt","jas.txt")           # rename.
+# os.remove("blank.txt")                     # remove file 
+
+'create new folder'
+# os.mkdir("Quest.txt")                      # create new folder
+
+
+
+'Nexted Folder'
+# os.makedirs('One/Two/Three')               # create a Nexted Folder
+
+
+'Remove Folder'
+# os.rmdir("One")                         # single folder only
+
+
+
+
+
+
+
+
+# import shutil
+# shutil.rmtree("One")                      # remove folder and inside files and others full
--- 6_python/Module_5/random_module.py
+++ 6_python/Module_5/random_module.py
@@ -73,7 +73,7 @@ from random import *
 
 
 
-'shuffle'
+'shuffle'           # only list
 
 # students =['jasil','shakir','yaseen','niyas']
 # shuffle(students)
@@ -92,6 +92,137 @@ from random import *
 
 'sample'
 
-students =['jasil','shakir','yaseen','niyas']
+# students =['jasil','shakir','yaseen','niyas']
 
-print(sample(students,k=3))
+# print(sample(students,k=3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'uniform'
+
+# students =['jasil','shakir','yaseen','niyas']
+
+# value = uniform(0,5)
+# print(value)                                      # 2.3328297142858374
+
+# rounded_value=round(value,2)
+# print(rounded_value)                              # 2.33
+
+
+
+
+
+
+
+
+'Simple Guss game'
+
+# value = randint(0,10)
+# user = int(input('Guss a number (1-9) : '))
+# if user==value:
+#     print("Your win !")
+# else:
+#     print("Your Fail")
+
+# print('Random num :',value)
+
+
+
+
+
+
+
+
+
+# value = randint(0,100)
+# user = int(input('Guss a number (1-99) : '))
+
+# if value < user:
+#     print("number Too low")
+# elif value > user:
+#     print("To High")
+# else:
+#     print("congrats your Win !!")
+
+# print(value)
+
+
+
+
+
+
+
+
+
+
+
+'Head or tail'
+
+# result = choice(['Head','Tail'])
+# print(f'congrats the Result is : {result}')
+
+
+
+
+
+
+
+
+'question picker'
+
+# question = ['whats is python','Features of pythom','Current version of python']
+# # selected = choice
+# print(question)
+
+# for i in question:
+#     print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'OTP'
+
+# from random import*
+
+# num = randint(1,999999)
+# otp = f'{num:06d}'
+# print(f'OTP : {otp}')
+
+
+
+
+
+
+
+
+
+
