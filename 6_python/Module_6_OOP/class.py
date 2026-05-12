427b0f7febf44e24780b53bdb1359a2f2ab6a8e2
Jasilxmhd <jasilmuhammed25@gmail.com>
2026-05-12 12:54:54 +0530

    12 May 26

---
+++ 6_python/Module_6_OOP/class.py
@@ -0,0 +1,44 @@
+
+
+# 'Class' 
+
+# class student:            # class
+#     pass
+
+
+
+
+# 'object'
+
+# Jasil = student()         # object
+# print(id(Jasil))
+
+
+# 'object'
+
+# Yaseen = student()        # object
+# print(id(Yaseen))
+
+
+
+
+
+
+
+
+
+
+class Student:                                              # class
+
+    '''Methods'''
+    def exam(self):                                         # method                  
+        print("Exam conducted on 08/05/2026")
+    
+
+jasil = Student()                                           # object
+yaseen = Student()                                          # object
+
+
+jasil.exam()                                                # Exam conducted on 08/05/2026
+yaseen.exam()                                               # Exam conducted on 08/05/2026
+
