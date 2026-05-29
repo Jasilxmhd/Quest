87882a15fad59a7392b1841be87316d4ff18b371
Jasilxmhd <jasilmuhammed25@gmail.com>
2026-05-29 20:17:16 +0530

    29 May 26

---
+++ 6_python/Module_6_OOP/7_exception_handling.py
@@ -0,0 +1,85 @@
+
+"Exception Handling"
+
+# a = int(input("Enter a number : "))
+# b = int(input("Enter another number : "))
+
+# c = a/b
+
+# print(c)
+
+
+'try '
+
+# try:
+#     a = int(input("Enter a number : "))
+#     b = int(input("Enter another number : "))
+
+#     c = a/b
+
+#     print(c)
+# except:
+#     print('Please enter valid input !!')
+
+
+
+
+
+'try , except , else'
+
+# try:
+#     a = int(input("Enter a number : "))
+#     b = int(input("Enter another number : "))
+#     c = a/b
+
+# except:
+#     print('Please enter valid input !!')
+
+# else:
+#     print(c)
+
+
+
+
+
+
+
+
+'finally'
+
+# try:
+#     a = int(input("Enter a number : "))
+#     b = int(input("Enter another number : "))
+#     c = a/b
+
+# except:
+#     print('Please enter valid input !!')
+
+# else:
+#     print(c)
+
+# finally:
+#     print("End of the progarm")
+
+
+
+
+
+
+
+
+
+'while True using exception'
+
+while True:
+    try:
+        a = int(input("Enter a number : "))
+        b = int(input("Enter another number : "))
+        c = a/b
+
+    except:
+        print('Please enter valid input !!')
+
+    else:
+        print(c)
+        break
