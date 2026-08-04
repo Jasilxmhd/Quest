e273fbf945ac9a1424406da2877fa4efa924a0fd
Jasilxmhd <jasilmuhammed25@gmail.com>
2026-08-04 14:32:50 +0530

    04 Aug 26

--- 8_Django/2_work/work3/SocialMedia/accounts/urls.py
+++ 8_Django/2_work/work3/SocialMedia/accounts/urls.py
@@ -6,4 +6,16 @@ urlpatterns = [
     path('register/', views.register, name='registerpage'),
     path('login/', views.user_login, name='loginpage'),
     path('home/', views.home, name='home'),
+    path('logout/', views.logout_view, name='logout'),
+
+
+
+    path('session/create', views.create_session),
+    path('session/get', views.get_session),
+    path('session/delete', views.clear_session),
+    
+    path('cookie/create', views.create_cookie),
+    path('cookie/get', views.get_cookie),
+    path('cookie/delete', views.delete_cookie),
+
 ]
--- 8_Django/2_work/work3/SocialMedia/accounts/views.py
+++ 8_Django/2_work/work3/SocialMedia/accounts/views.py
@@ -2,7 +2,22 @@ from django.shortcuts import render,redirect
 from .forms import CustomUserForm,LoginForm
 from django.http import HttpResponse
 from  django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
+
+
+
+
+
+def welcome(request):
+    return render(request, 'welcome.html')
+
+
+
+
+
+
 
 def register(request):
     form =CustomUserForm(request.POST)
@@ -19,12 +34,16 @@ def register(request):
     return render(request,'register.html',{'form':form})
 
 
+@login_required(login_url='login')
 def home(request):
-    return HttpResponse("This is Home Page.....")
+    return render(request,'home.html')
 
 
 
 def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    
     if request.method == 'POST':
         login_form=LoginForm(request.POST)
         
@@ -32,21 +51,74 @@ def user_login(request):
             username = login_form.cleaned_data.get('username')
             password = login_form.cleaned_data.get('password')
             
-            user =authenticate(request,username=username,password=password)
+            user = authenticate(request,username=username,password=password)
             
             if user is not None:
+                request.session['username'] = username
                 login(request,user)
                 return redirect('home')          
             else:
                 return HttpResponse("Invalid USer")
     else:
         login_form=LoginForm()
-    return render(request,'login.html',{'login_form':login_form})
+    u = request.session.get('username')
+    return render(request,'login.html',{'login_form':login_form , 'user':u })
 
 
 
 
 
 
-def welcome(request):
-    return render(request, 'welcome.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect(user_login)
+
+
+
+
+
+
+
+# Session
+
+
+
+def create_session(request):
+    request.session['username'] = 'john'                              # username is key
+    request.session['email'] = 'john@example.com'
+    return HttpResponse("Session Created")
+
+
+
+
+def get_session(request):
+    u_name = request.session.get('username')
+    return HttpResponse(u_name)
+
+
+
+
+def clear_session(request):
+    request.session.flush()                                            # remove full data
+    return HttpResponse('All session data removed')
+
+
+
+# cookie
+
+
+def create_cookie(request):
+    response = HttpResponse("cookie created")
+    response.set_cookie('username', 'john' , max_age=3600)  # 1 hour
+    return response
+
+def get_cookie(request):
+    username = request.COOKIES.get('username')
+    return HttpResponse(username or 'NO cookie found')
+
+def delete_cookie(request):
+    response =HttpResponse("cookie deleted")
+    response.delete_cookie("username")
+    return response
