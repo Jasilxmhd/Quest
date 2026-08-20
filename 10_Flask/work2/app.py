89aa4625868db99702acaaddffd6735daa7d6cfc
Jasilxmhd <jasilmuhammed25@gmail.com>
2026-08-20 23:10:27 +0530

    20 Aug 26

--- 10_Flask/work2/app.py
+++ 10_Flask/work2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy                                                                                                                        # type: ignore[reportMissingImports]
 
 app = Flask(__name__)
@@ -8,6 +8,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = (
 )
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 
 db = SQLAlchemy(app)
 
@@ -49,6 +50,76 @@ with app.app_context():
 @app.route("/")
 def home():
     return "Flask + MySQL Working !!! "
+
+
+
+
+
+
+
+
+@app.route("/index")
+def index():
+    students = Student.query.all()
+
+    return render_template("index.html",students=students)
+
+
+@app.route("/add", methods=["GET" , "POST"])
+def add_student():
+
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        course = request.form["course"]
+        age = request.form["age"]
+
+
+        student = Student(
+            name = name,
+            email = email,
+            course = course,
+            age = age
+        )
+        db.session.add(student)
+        db.session.commit()
+        return redirect(url_for("index"))
+    return render_template("add_student.html")
+
+
+
+
+
+@app.route("/update/<int:id>", methods = ["GET","POST"])
+def update_student(id):
+
+    student = Student.query.get(id)
+
+    if request.method == "POST":
+        student.name = request.form["name"]
+        student.email = request.form["email"]
+        student.course = request.form["course"]
+        student.age = request.form["age"]
+
+        db.session.commit()
+        return redirect(url_for("index"))
+    return render_template("update_student.html")
+ 
+
+
+
+
+@app.route("/delete/<int:id>", methods = ["GET","POST"])
+def delete_student(id):
+
+    student = Student.query.get(id)
+    db.session.delete(student)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+
+
 
 
 if __name__ == "__main__":
